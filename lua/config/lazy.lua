df4d6b31daca321a071164abafee756c7a34fd37
Ruben Rundström <ruben.rundstrom@gmail.com>
2026-02-20 16:27:06 +0100

    Jork it crazy style

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,54 +23,65 @@ vim.g.maplocalleader = " "
 
 -- Diagnostics
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "●", -- could also be "■", "▎", "", etc.
-  },
-  signs = true,
-  underline = true,
-  update_in_insert = true,
-  severity_sort = true,
+	virtual_text = {
+		prefix = "●", -- could also be "■", "▎", "", etc.
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+	severity_sort = true,
 })
 
 if vim.loop.os_uname().sysname == "Darwin" then
-  require("config.mac")
+	require("config.mac")
 end
 
-
 -- Tabs
-vim.opt.expandtab = false      -- Use actual tab characters
-vim.opt.tabstop = 4            -- Display width of a tab character
-vim.opt.shiftwidth = 4         -- Indent amount when using >>, <<
-vim.opt.softtabstop = 4        -- How many spaces a <Tab> feels like
-
+vim.opt.expandtab = false -- Use actual tab characters
+vim.opt.tabstop = 4 -- Display width of a tab character
+vim.opt.shiftwidth = 4 -- Indent amount when using >>, <<
+vim.opt.softtabstop = 4 -- How many spaces a <Tab> feels like
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
-
 
 vim.cmd("colorscheme tokyonight-night")
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "markdown_inline" },
-  callback = function()
-    pcall(vim.treesitter.start, 0, "markdown")
-    pcall(vim.treesitter.start, 0, "markdown_inline")
-  end,
+	pattern = { "markdown", "markdown_inline" },
+	callback = function()
+		pcall(vim.treesitter.start, 0, "markdown")
+		pcall(vim.treesitter.start, 0, "markdown_inline")
+	end,
 })
 
-
-vim.opt.number = true            -- Shows the absolute number on the current line
-vim.opt.relativenumber = true    -- Shows relative numbers on other lines
+vim.opt.number = true -- Shows the absolute number on the current line
+vim.opt.relativenumber = true -- Shows relative numbers on other lines
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(args)
+		-- 0
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 1
+			buffer = args.buf,
+			callback = function()
+				-- 2 + 5
+				vim.lsp.buf.format({ async = false, id = args.data.client_id })
+			end,
+		})
+	end,
+})
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,17 +1,62 @@
+-- ~/.config/nvim/lua/plugins/mason-lspconfig.lua
 return {
   "williamboman/mason-lspconfig.nvim",
-  dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig","hrsh7th/cmp-nvim-lsp",},
+  lazy = false,  -- load at startup
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+  },
   config = function()
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    require('mason-lspconfig').setup({	
-		handlers = {
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-			end
-		},
-	})
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local mlsp = require("mason-lspconfig")
+
+    -- 1️⃣ Setup Mason LSPConfig
+    mlsp.setup({
+      ensure_installed = { "clangd" }, -- auto-install clangd
+      handlers = {
+        function(server_name)
+          local opts = { capabilities = capabilities }
+
+          -- 🔧 Custom clangd setup
+          if server_name == "clangd" then
+            opts.cmd = {
+              "clangd",
+              "-style={BasedOnStyle: Google, IndentWidth: 4, UseTab: Always, TabWidth: 4}",
+              "--fallback-style=Google",
+              "--offset-encoding=utf-16",
+              "--clang-tidy",
+              "--enable-config",
+            }
+            opts.init_options = {
+              fallbackFlags = {
+                "-style={BasedOnStyle: Google, IndentWidth: 4, UseTab: Always, TabWidth: 4}",
+              },
+            }
+          end
+
+          -- 🔹 Attach formatting autocmd for all LSPs
+          opts.on_attach = function(client, bufnr)
+            -- Optional: keep existing on_attach
+            if client.server_capabilities.documentFormattingProvider then
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("LspFormat", { clear = false }),
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format({ async = false, id = client.id })
+                end,
+              })
+            end
+          end
+
+          -- Setup the LSP
+          lspconfig[server_name].setup(opts)
+        end,
+      },
+    })
+
+    print("✅ Mason-LSPConfig setup complete")
   end,
 }
 
